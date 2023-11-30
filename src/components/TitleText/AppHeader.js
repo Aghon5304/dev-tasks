07e32dde941d2ff999e47156ffebf078055f062a
Aghon5304 <matusoliwier@gmail.com>
2023-11-30 19:10:23 +0100

    feature/dev-task-1-and-2

---
+++ src/components/TitleText/AppHeader.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const AppHeader =() => (
+    <h2>Aghon5304-dev tasks</h2>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { AppHeader } from '../components/TitleText/appheader';
 import { TitleText } from '../components/TitleText/TitleText';
 
 const IndexPage = () => (
   <>
     <TitleText />
+    <AppHeader />
   </>
 );
 
