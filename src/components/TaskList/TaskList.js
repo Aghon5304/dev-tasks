b44714a18d68fc13ae83fb462217675938104b1a
Oliwier Matus <matusoliwier@gmail.com>
2023-12-04 21:08:06 +0100

    Merge branch 'feature/dev-task-6'

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,32 @@
+import React,{useState} from "react";
+
+import { sampleTasks } from "../../pages";
+
+const TaskList = ()=>{
+    let [tasks,setTasks] = useState(sampleTasks);
+
+    const handleCheckbox = (index) =>{
+        let newTasks=[...tasks];
+        newTasks[index].completed=!newTasks[index].completed;
+        setTasks(newTasks);
+    }
+
+    const handleClick = (index)=>{
+        let newTasks = [...tasks];
+        newTasks.splice(index,1)
+        setTasks(newTasks)
+    }
+
+    const taskElements = tasks.map((task,index)=>(
+        <li key={index} style={task.completed ? {textDecoration:"line-through", color:"red"}:{textDecoration:"none", color:"green"}}>
+            <input type="checkbox" checked={task.completed} onChange={()=>handleCheckbox(index)}/>
+            {task.text}
+            <button onClick={()=>handleClick(index)}>Remove</button>
+        </li>
+    ));
+
+    return(
+            <ol>{taskElements}</ol>
+    )
+}
+export default TaskList
